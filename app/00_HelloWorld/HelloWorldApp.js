d078e2d4ba0d50487031224a80e26a7694180318
pandaguo <panda.guo@gservfocus.com>
2017-06-14 15:56:27 +0800

    配置基础工程完毕,HelloWorld示例

---
+++ app/00_HelloWorld/HelloWorldApp.js
@@ -0,0 +1,18 @@
+/**
+ * Created by panda.guo on 2017/6/14.
+ */
+import React, {Component}from 'react';
+import {Text} from 'react-native';
+
+export default class HelloWorldApp extends Component {
+
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (<Text>Hello World!</Text>);
+    }
+
+
+}
---
+++ app/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by panda.guo on 2017/6/14.
+ */
+import React, {Component} from "react";
+import {AppRegistry} from "react-native";
+
+import HelloWorldApp from './00_HelloWorld/HelloWorldApp';
+
+AppRegistry.registerComponent('HelloWorld', () => HelloWorldApp);
